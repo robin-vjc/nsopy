- implemented averaging for UFGM - added tests for averaged variantes of UGMs - fixed notebooks after changes - polished directory tree

methods/universal_gradient_methods.py
@@ -357,7 +357,7 @@ class UniversalFGM(DualMethod, Observable):
     Note that the algorithm is written for the maximization of a convex function, while in the duality
     framework we maximize a concave fct. Hence, f(x) := -d(lambda)
     """
-    def __init__(self, oracle, projection_function, dimension=0, epsilon=UGM_DEFAULT_EPSILON):
+    def __init__(self, oracle, projection_function, dimension=0, epsilon=UGM_DEFAULT_EPSILON, averaging=False):
         super(UniversalFGM, self).__init__()
 
         self.desc = 'UFGM, $\epsilon = {}$'.format(epsilon)
@@ -368,27 +368,39 @@ class UniversalFGM(DualMethod, Observable):
         self.iteration_number = 1
         self.oracle_calls = 0
 
-        self.d_k = np.zeros(1, dtype=float)
+        self.d_hat_k = np.zeros(1, dtype=float)
         if dimension == 0:
-            self.lambda_k = self.projection_function(0)
-            self.dimension = len(self.lambda_k)
+            self.lambda_hat_k = self.projection_function(0)
+            self.dimension = len(self.lambda_hat_k)
         else:
             self.dimension = dimension
-            self.lambda_k = self.projection_function(np.zeros(self.dimension, dtype=float))
-        self.x_k = 0
+            self.lambda_hat_k = self.projection_function(np.zeros(self.dimension, dtype=float))
+        self.x_hat_k = 0
 
         # specific to U-PGM
-        self.diff_d_k = 0
+        self.diff_d_hat_k = 0
         self.L_k = float(UGM_DEFAULT_L_0)  # if you use something else, make sure it's a float!
         self.epsilon = float(epsilon)
         self.i_k = 0
-        self.phi_k = copy.deepcopy(self.lambda_k)
+        self.phi_k = copy.deepcopy(self.lambda_hat_k)
 
-        self.y_k = copy.deepcopy(self.lambda_k)
+        self.y_k = copy.deepcopy(self.lambda_hat_k)
         self.A_k = 0
-        self.a_k = copy.deepcopy(self.lambda_k)
+        self.a_k = copy.deepcopy(self.lambda_hat_k)
         self.tau_k = 0
         self.v_k = 0
+
+        self.averaging = averaging
+        if self.averaging:
+            self.lambda_k = self.y_k
+            self.d_k = self.d_hat_k
+            self.diff_d_k = self.diff_d_hat_k
+            self.x_k = self.x_hat_k
+        else:
+            self.lambda_k = self.lambda_hat_k
+            self.d_k = self.d_hat_k
+            self.diff_d_k = self.diff_d_hat_k
+            self.x_k = self.x_hat_k
 
         # for record keeping
         self.method_name = 'UFGM'
@@ -443,19 +455,31 @@ class UniversalFGM(DualMethod, Observable):
 
         self.iteration_number += 1
         # Perform step
-        self.lambda_k = lambda_kp_ik
+        self.lambda_hat_k = lambda_kp_ik
         self.y_k = y_kp_ik
         self.a_k = a_kp_ik
         self.tau_k = tau_k_ik
         self.A_k = self.A_k + self.a_k
         self.L_k = 2**(i_k-1)*self.L_k
-        self.phi_k += self.a_k * self.diff_d_k
+        self.phi_k += self.a_k * self.diff_d_hat_k
 
         # Record additional information about iterate
-        self.d_k= d_kp_ik
-        self.diff_d_k = diff_kp_ik
-        self.x_k = x_kp_ik
+        self.d_hat_k= d_kp_ik
+        self.diff_d_hat_k = diff_kp_ik
+        self.x_hat_k = x_kp_ik
         self.i_k = i_k
+
+        # Calculate ouputs depending on whether averaging is active or not:
+        if self.averaging:
+            # we have an additional oracle call
+            self.lambda_k = self.y_k
+            self.x_k, self.d_k, self.diff_d_k = self.oracle(self.lambda_k)
+            self.oracle_calls += 1
+        else:
+            self.x_k = self.x_hat_k
+            self.d_k = self.d_hat_k
+            self.lambda_k = self.lambda_hat_k
+            self.diff_d_k = self.diff_d_hat_k
 
         # log signal to any observers connected
         self.notify_observers()

tests/test_universal_gradient_methods.py
@@ -1,8 +1,10 @@
 from __future__ import print_function
+
 import numpy as np
-from methods.universal_gradient_methods import UniversalPGM, UniversalDGM, UniversalFGM
 from methods.method_loggers import GenericDualMethodLogger, DualDgmFgmMethodLogger
-from examples.analytical_oracles import AnalyticalExampleInnerProblem, SecondAnalyticalExampleInnerProblem, ConstrainedDualAnalyticalExampleInnerProblem
+from methods.universal_gradient_methods import UniversalPGM, UniversalDGM, UniversalFGM
+from tests.analytical_oracles import AnalyticalExampleInnerProblem, SecondAnalyticalExampleInnerProblem, ConstrainedDualAnalyticalExampleInnerProblem
+
 
 #############
 # PGM TESTS #
@@ -21,16 +23,42 @@ def test_UPGM_on_analytical_example():
 
     logger = GenericDualMethodLogger(dual_method)
 
-    for iteration in range(50):
-        print(dual_method.S_k)
-        print(dual_method.lambda_k)
-        print(dual_method.d_k)
+    for iteration in range(10):
+        # print(dual_method.S_k)
+        # print(dual_method.lambda_k)
+        # print(dual_method.d_k)
         dual_method.dual_step()
 
     # Method should end close to lambda*
     np.testing.assert_allclose(logger.lambda_k_iterates[-1], np.array([1., 1.]), rtol=1e-1, atol=0)
     # with value close to dual optimum
     np.testing.assert_allclose(logger.d_k_iterates[-1], -0.5, rtol=1e-1, atol=0)
+
+
+
+def test_averaged_UPGM_on_analytical_example():
+    print('# Test averaged UPGM on Analytical Example')
+    # see definition of AnalyticalExampleInnerProblem for problem and solution statement
+    analytical_inner_problem = AnalyticalExampleInnerProblem()
+
+    dual_method = UniversalPGM(analytical_inner_problem.oracle,
+                               analytical_inner_problem.projection_function,
+                               dimension=analytical_inner_problem.dimension,
+                               epsilon=0.01,
+                               averaging=True)
+
+    logger = GenericDualMethodLogger(dual_method)
+
+    for iteration in range(50):
+        # print(dual_method.S_k)
+        # print(dual_method.lambda_k)
+        # print(dual_method.d_k)
+        dual_method.dual_step()
+
+    # When averaging is turned on, method is much slower. Method should end close to lambda* but get to about [0.3, 0.7]
+    np.testing.assert_allclose(logger.lambda_k_iterates[-1], np.array([0.3, 0.7]), atol=0.1)
+    # should be close to optimal value (-0.5) but is at -0.8
+    np.testing.assert_allclose(logger.d_k_iterates[-1], -0.8, atol=0.1)
 
 
 def test_UPGM_on_second_analytical_example():
@@ -71,8 +99,8 @@ def test_UPGM_on_third_analytical_example_non_zero_start():
     logger = GenericDualMethodLogger(dual_method)
 
     for iteration in range(5):
-        print(dual_method.lambda_k)
-        print(dual_method.lambda_tilde_k)
+        # print(dual_method.lambda_k)
+        # print(dual_method.lambda_tilde_k)
         # print(dual_method.d_k)
         dual_method.dual_step()
 
@@ -112,6 +140,31 @@ def test_UDGM_on_analytical_example():
     np.testing.assert_allclose(logger.lambda_k_iterates[-1], np.array([1., 1.]), rtol=1e-1, atol=0)
     # with value close to dual optimum
     np.testing.assert_allclose(logger.d_k_iterates[-1], -0.5, rtol=1e-1, atol=0)
+
+
+def test_averaged_UDGM_on_analytical_example():
+    print('# Test UDGM on Analytical Example')
+    # see definition of AnalyticalExampleInnerProblem for problem and solution statement
+    analytical_inner_problem = AnalyticalExampleInnerProblem()
+
+    dual_method = UniversalDGM(analytical_inner_problem.oracle,
+                               analytical_inner_problem.projection_function,
+                               dimension=analytical_inner_problem.dimension,
+                               epsilon=0.01,
+                               averaging=True)
+    # epsilon=0.5)
+
+    logger = GenericDualMethodLogger(dual_method)
+
+    for iteration in range(20):
+        # print(dual_method.lambda_k)
+        # print(dual_method.d_k)
+        dual_method.dual_step()
+
+    # When averaging is turned on, method is much slower. Method should end close to lambda* but get to about [0.7, 0.9]
+    np.testing.assert_allclose(logger.lambda_k_iterates[-1], np.array([0.7, 0.9]), atol=0.1)
+    # should be close to optimal value (-0.5) but is at -0.8
+    np.testing.assert_allclose(logger.d_k_iterates[-1], -0.6, atol=0.1)
 
 
 def test_UDGM_on_second_analytical_example():
@@ -193,6 +246,31 @@ def test_UFGM_on_analytical_example():
     np.testing.assert_allclose(logger.d_k_iterates[-1], -0.5, rtol=1e-1, atol=0)
 
 
+def test_averaged_UFGM_on_analytical_example():
+    print('# Test Averaged UFGM on Analytical Example')
+    # see definition of AnalyticalExampleInnerProblem for problem and solution statement
+    analytical_inner_problem = AnalyticalExampleInnerProblem()
+
+    dual_method = UniversalFGM(analytical_inner_problem.oracle,
+                               analytical_inner_problem.projection_function,
+                               dimension=analytical_inner_problem.dimension,
+                               epsilon=0.1,
+                               averaging=False)
+
+    logger = GenericDualMethodLogger(dual_method)
+
+    for iteration in range(40):
+        print(dual_method.lambda_k)
+        # print dual_method.d_k
+        dual_method.dual_step()
+
+    # Method should end close to lambda*
+    np.testing.assert_allclose(logger.lambda_k_iterates[-1], np.array([1., 1.]), rtol=1e-1, atol=0)
+    # with value close to dual optimum
+    np.testing.assert_allclose(logger.d_k_iterates[-1], -0.5, rtol=1e-1, atol=0)
+
+
+
 def test_UFGM_on_second_analytical_example():
     print('# Test UFGM on Second Analytical Example (1 eq, 1 ineq)')
     # see definition of AnalyticalExampleInnerProblem for problem and solution statement
@@ -229,7 +307,7 @@ def test_UFGM_on_third_analytical_example_non_zero_start():
                                epsilon=0.01)
 
     # we set the initial point somewhere not 0
-    dual_method.lambda_k = dual_method.projection_function(np.array([-2,2]))
+    dual_method.lambda_hat_k = dual_method.projection_function(np.array([-2, 2]))
     # print(dual_method.lambda_k)
     logger = DualDgmFgmMethodLogger(dual_method)
 