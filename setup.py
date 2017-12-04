3317bb72a811454b02130da901697e820a0cfa6e
vujanicr <robin@acfr.usyd.edu.au>
2017-12-04 18:36:51 +1100

    polish setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
 from setuptools import setup, find_packages
 
-with open('./requirements.txt') as f:
-    required = f.read().splitlines()
+# with open('./requirements.txt') as f:
+#     required = f.read().splitlines()
+
+REQUIRES = [
+    'colorama >= 0.3.7',
+    'numpy >= 1.11.2',
+    'pandas >= 0.19.0',
+    'python-dateutil >= 2.5.3',
+    'pytz >= 2016.7',
+    'six >= 1.10.0',
+]
 
 setup(
     name='nsopy',
@@ -9,9 +18,10 @@ setup(
     description='Non-smooth optimization for Python',
     author='Robin Vujanic',
     author_email='vjc.robin@gmail.com',
-    install_requires=required,
+    install_requires=REQUIRES,
     url='https://github.com/robin-vjc/nsopy',  # use the URL to the github repo
     download_url='https://github.com/robin-vjc/nsopy/archive/0.1.tar.gz',
     keywords=['non-smooth', 'distributed', 'optimization', 'python'],
     packages=find_packages(),
 )
+
