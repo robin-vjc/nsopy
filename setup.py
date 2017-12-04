f4a19760deac65bd1530cd47204cc6a1a3e774f2
vujanicr <robin@acfr.usyd.edu.au>
2017-12-04 18:11:35 +1100

    setup.py polish

--- setup.py
+++ setup.py
@@ -7,9 +7,11 @@ setup(
     name='nsopy',
     version='1.0',
     description='Non-smooth optimization for Python',
-    # url='http://github.com/storborg/funniest',
     author='Robin Vujanic',
     author_email='vjc.robin@gmail.com',
     install_requires=required,
+    url='https://github.com/robin-vjc/nsopy',  # use the URL to the github repo
+    download_url='https://github.com/robin-vjc/nsopy/archive/0.1.tar.gz',
+    keywords=['non-smooth', 'distributed', 'optimization', 'python'],
     packages=find_packages(),
 )
